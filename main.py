149f6249a7c917bf8ad0bf131c31920b265479be
laurentbruneteau <laurentp200375@me.com>
2022-12-16 15:06:12 +0000

    add gunicorn.py file

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from flask_login import UserMixin, login_user, LoginManager, login_required, cur
 from forms import CreatePostForm, CreateUserForm, CreateLoginForm, CreateCommentForm
 from flask_gravatar import Gravatar
 from functools import wraps
+import gunicorn
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
